ce29c800348d823c2b196f0cf627f9489e1993e1
sgyt <872147302@qq.com>
2019-04-08 11:08:28 +0800

    book have jar 2019/4/8

---
+++ src/main/java/common/package01/RandomTest.java
@@ -0,0 +1,16 @@
+package common.package01;
+
+import java.util.Random;
+
+/**
+ * @Author:sgyt
+ * @Description:
+ * @Date:2019/4/8 9:43
+ */
+public class RandomTest {
+    public static void main(String[] args) {
+        Random a = new Random();
+        a.setSeed(5);
+        
+    }
+}
